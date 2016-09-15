c5d555406ee6e27b89df7b6058312c328949705a
John <John@JOHN>
2016-09-15 11:12:20 +0200

    Add project files.

---
+++ 05_Temperature/ITemperature.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _05_Temperature
+{
+	internal interface ITemperature
+	{
+		double Convert();
+	}
+	internal class TemperatureFactory
+	{
+		internal static ITemperature Get(Unit E)
+		{
+			if(E == Unit.Celcius)
+			{
+				return new Celcius();
+			}else
+			{
+				return new Fahrenheit();
+			}
+		}
+		
+	}
+
+	internal class Celcius : ITemperature
+	{
+		double Convert(double T)
+		{
+			return (5 * T - 160) / 9;
+		}
+	}
+	internal class Fahrenheit : ITemperature
+	{
+		double Convert(double T)
+		{
+			return T * 9 / 5 + 32;
+		}
+	}
+}
---
+++ 05_Temperature/Temperature.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Temperature
+{
+	public enum Unit
+	{
+		Celcius,
+		Fahrenheit
+	};
+	public class Temperature
+	{
+		public double Celcius;
+		public double Fahrenheit;
+
+
+		public Temperature()
+		{
+
+		}
+		public Temperature(Unit E, double T)
+		{
+			if (Unit.Celcius == E)
+			{
+				Celcius = T;
+			} else if (Unit.Fahrenheit == E)
+			{
+				Fahrenheit = T;
+			}
+		}
+		
+		public static double FahrenheitToCelcius(double t)
+		{
+			return (5*t - 160) / 9;
+		}
+
+		public static double CelciusToFahrenheit(double t)
+		{
+			return t * 9 / 5 + 32;
+		}
+
+	}
+}
